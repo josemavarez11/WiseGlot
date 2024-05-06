dc050c839ddc657433f4e428fa0713eeecb50147
Misa24jr <misa24jr@gmail.com>
2024-05-06 08:57:11 -0400

    Nuevo Proyecto

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'WiseGlot',
+  webDir: 'www'
+};
+
+export default config;
---
+++ src/app/app.routes.ts
@@ -0,0 +1,12 @@
+import { Routes } from '@angular/router';
+
+export const routes: Routes = [
+  {
+
+  },
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full',
+  },
+];
