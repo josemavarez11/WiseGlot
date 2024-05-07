e7527173579520a17fe34d40471d0561e3428e73
Misa24jr <misa24jr@gmail.com>
2024-05-07 11:28:40 -0400

    Login y welcome son totalmente adaptables a cualquier dispositivo, se ha añadido un logo y un fondo de pantalla para la app.

--- src/app/views/login-view/login-view.page.ts
+++ src/app/views/login-view/login-view.page.ts
@@ -1,14 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
-
+import { IonicModule } from '@ionic/angular';
+import { RouterLink } from '@angular/router';
 @Component({
   selector: 'app-login-view',
   templateUrl: './login-view.page.html',
   styleUrls: ['./login-view.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [IonicModule, CommonModule, FormsModule, RouterLink]
 })
 export class LoginViewPage implements OnInit {
 
