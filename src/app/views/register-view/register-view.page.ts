be7e22e668a818efbf9ebb2d0f43c29a86041d32
Misa24jr <misa24jr@gmail.com>
2024-05-08 12:19:31 -0400

    Vistas Login, register y welcome funcionando correctamente y adaptables a cualquier telefono

--- src/app/views/register-view/register-view.page.ts
+++ src/app/views/register-view/register-view.page.ts
@@ -2,13 +2,14 @@ import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-register-view',
   templateUrl: './register-view.page.html',
   styleUrls: ['./register-view.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, RouterLink]
 })
 export class RegisterViewPage implements OnInit {
 
