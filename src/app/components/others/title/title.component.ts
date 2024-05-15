f505c8a2ff3ad3096a321aff7c37ba27b96e084f
Misa24jr <misa24jr@gmail.com>
2024-05-15 10:21:04 -0400

    Componente titulo

---
+++ src/app/components/others/title/title.component.ts
@@ -0,0 +1,17 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-title',
+  templateUrl: './title.component.html',
+  styleUrls: ['./title.component.scss'],
+  standalone: true
+})
+export class TitleComponent  implements OnInit {
+  @Input() title: string = '';
+  @Input() subtitle: string = '';
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
--- src/app/views/welcome-view/welcome-view.page.ts
+++ src/app/views/welcome-view/welcome-view.page.ts
@@ -3,13 +3,14 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { RouterLink } from '@angular/router';
 import { IonicModule } from '@ionic/angular';
-
+// Components
+import { TitleComponent } from 'src/app/components/others/title/title.component';
 @Component({
   selector: 'app-welcome-view',
   templateUrl: './welcome-view.page.html',
   styleUrls: ['./welcome-view.page.scss'],
   standalone: true,
-  imports: [IonicModule,CommonModule, FormsModule, RouterLink]
+  imports: [IonicModule,CommonModule, FormsModule, RouterLink, TitleComponent]
 })
 export class WelcomeViewPage implements OnInit {
 
