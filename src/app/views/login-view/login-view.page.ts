560bc1e49b149ea44f6ae9645c17c7a4f40e250e
Misa24jr <misa24jr@gmail.com>
2024-05-15 10:40:27 -0400

    Cambios en los componentes

--- src/app/views/login-view/login-view.page.ts
+++ src/app/views/login-view/login-view.page.ts
@@ -3,12 +3,14 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
 import { RouterLink } from '@angular/router';
+// Components
+import { TitleLrComponent } from 'src/app/components/others/title-lr/title-lr.component';
 @Component({
   selector: 'app-login-view',
   templateUrl: './login-view.page.html',
   styleUrls: ['./login-view.page.scss'],
   standalone: true,
-  imports: [IonicModule, CommonModule, FormsModule, RouterLink]
+  imports: [IonicModule, CommonModule, FormsModule, RouterLink, TitleLrComponent]
 })
 export class LoginViewPage implements OnInit {
 
--- src/app/views/register-view/register-view.page.ts
+++ src/app/views/register-view/register-view.page.ts
@@ -3,13 +3,15 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
 import { RouterLink } from '@angular/router';
+// Components
+import { TitleLrComponent } from 'src/app/components/others/title-lr/title-lr.component';
 
 @Component({
   selector: 'app-register-view',
   templateUrl: './register-view.page.html',
   styleUrls: ['./register-view.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, RouterLink]
+  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, RouterLink, TitleLrComponent]
 })
 export class RegisterViewPage implements OnInit {
 
