613af1d14bc0d91d484c7c794605c219d233edb3
Misa24jr <misa24jr@gmail.com>
2024-06-05 15:20:52 -0400

    Btn del home

--- src/app/views/home-view/home-view.page.ts
+++ src/app/views/home-view/home-view.page.ts
@@ -1,20 +1,27 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Router } from '@angular/router';
 import { FormsModule } from '@angular/forms';
 import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+
+// Components
+import { BtnAuthComponent } from 'src/app/components/buttons/btn-auth/btn-auth.component';
 
 @Component({
   selector: 'app-home-view',
   templateUrl: './home-view.page.html',
   styleUrls: ['./home-view.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, FormsModule]
+  imports: [IonContent, IonHeader, IonTitle, IonToolbar, FormsModule, BtnAuthComponent]
 })
 export class HomeViewPage implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit() {
+  }
+
+  handleClick(): void {
+    this.router.navigate(['/login']);
   }
 
 }
