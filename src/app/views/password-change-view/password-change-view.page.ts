adfa8474daa05e761cf7c752789208dc3445a368
Misa24jr <misa24jr@gmail.com>
2024-06-11 07:30:45 -0400

    loading de chage-password lsito

--- src/app/views/password-change-view/password-change-view.page.ts
+++ src/app/views/password-change-view/password-change-view.page.ts
@@ -6,16 +6,26 @@ import { RouterLink, Router, NavigationEnd } from '@angular/router';
 import { TitleLrComponent } from 'src/app/components/others/title-lr/title-lr.component';
 import { MessageErrorComponent } from 'src/app/components/containers/message-error/message-error.component';
 import { ModalComponent } from 'src/app/components/others/modal/modal.component';
+import { LoadingComponent } from 'src/app/components/others/loading/loading.component';
 // Services
 import { AuthService } from 'src/services/auth.service';
-import { IonContent } from "@ionic/angular/standalone";
+import { IonContent } from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-password-change-view',
   templateUrl: './password-change-view.page.html',
   styleUrls: ['./password-change-view.page.scss'],
   standalone: true,
-  imports: [IonContent, FormsModule, RouterLink, TitleLrComponent, MessageErrorComponent, CommonModule, ModalComponent]
+  imports: [
+    IonContent,
+    FormsModule,
+    RouterLink,
+    TitleLrComponent,
+    MessageErrorComponent,
+    CommonModule,
+    ModalComponent,
+    LoadingComponent
+  ],
 })
 export class PasswordChangeViewPage implements OnInit {
   // Mostrar u ocultar la contraseña
@@ -25,6 +35,7 @@ export class PasswordChangeViewPage implements OnInit {
   showErrorMessage: boolean = false;
   errorMessage: string = '';
   showSuccessModal: boolean = false;
+  isLoading: boolean = false;
 
   constructor(private authService: AuthService, private router: Router) {
     this.router.events.subscribe((event) => {
@@ -50,48 +61,56 @@ export class PasswordChangeViewPage implements OnInit {
   }
 
   async handleClick(): Promise<void> {
-
+    this.isLoading = true;
     if (!this.password || !this.confirmPassword) {
       this.errorMessage = 'Please fill in all fields';
       this.showErrorMessage = true;
+      this.isLoading = false;
       return this.toggleErrorMessage();
     }
 
     if (this.password.length < 8) {
       this.errorMessage = 'Password must be at least 8 characters';
       this.showErrorMessage = true;
+      this.isLoading = false;
       return this.toggleErrorMessage();
     }
 
     if (this.password !== this.confirmPassword) {
       this.errorMessage = 'Passwords do not match';
       this.showErrorMessage = true;
+      this.isLoading = false;
       return this.toggleErrorMessage();
     }
 
     console.log('Password:', this.password);
 
     try {
-      const response = await fetch('https://wiseglot-api.onrender.com/auth/reset-password/', {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({
-          code: '',
-          email: '',
-          password: this.password,
-        })
-      });
+      const response = await fetch(
+        'https://wiseglot-api.onrender.com/auth/reset-password/',
+        {
+          method: 'POST',
+          headers: { 'Content-Type': 'application/json' },
+          body: JSON.stringify({
+            code: '',
+            email: '',
+            password: this.password,
+          }),
+        }
+      );
 
       if (response.status === 400) {
         const data = await response.json();
         this.errorMessage = data.error;
         this.showErrorMessage = true;
+        this.isLoading = false;
         return this.toggleErrorMessage();
       }
 
       if (response.status !== 200) {
         this.errorMessage = 'Unknown error. Try again later.';
         this.showErrorMessage = true;
+        this.isLoading = false;
         return this.toggleErrorMessage();
       }
 
@@ -100,6 +119,7 @@ export class PasswordChangeViewPage implements OnInit {
     } catch (error: any) {
       this.errorMessage = error.message;
       this.showErrorMessage = true;
+      this.isLoading = false;
       return this.toggleErrorMessage();
     }
   }
