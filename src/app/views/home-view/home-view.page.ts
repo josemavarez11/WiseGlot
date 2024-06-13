d1a1f95c4fcd331046687bd1055aec937630e5a0
Misa24jr <misa24jr@gmail.com>
2024-06-13 10:16:21 -0400

    Intentando hacer el modal de selección de idiomas

--- src/app/views/home-view/home-view.page.ts
+++ src/app/views/home-view/home-view.page.ts
@@ -2,26 +2,32 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { FormsModule } from '@angular/forms';
 import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
-
 // Components
 import { BtnAuthComponent } from 'src/app/components/buttons/btn-auth/btn-auth.component';
+import { ModalSelectionComponent } from 'src/app/components/others/modal-selection/modal-selection.component';
 
 @Component({
   selector: 'app-home-view',
   templateUrl: './home-view.page.html',
   styleUrls: ['./home-view.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, FormsModule, BtnAuthComponent]
+  imports: [IonContent, IonHeader, IonTitle, IonToolbar, FormsModule, BtnAuthComponent, ModalSelectionComponent]
 })
 export class HomeViewPage implements OnInit {
+  showModal: boolean = true;
 
   constructor(private router: Router) { }
 
-  ngOnInit() {
+  ngOnInit() {}
+
+  handleModalSelection(choice: string) {
+    this.showModal = false;
+    if (choice === 'volver') {
+      this.router.navigate(['/login']);
+    }
   }
 
-  handleClick(): void {
+  handleClick(){
     this.router.navigate(['/login']);
   }
-
 }
