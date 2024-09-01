603612fa476be768fcacc09675bce09bd827a654
josemavarez11 <josemmr311@gmail.com>
2024-09-01 15:48:02 -0400

    chat methods done pending list updated

--- src/app/views/chat-view/chat-view.page.ts
+++ src/app/views/chat-view/chat-view.page.ts
@@ -1,28 +1,85 @@
-import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+import { Component, OnInit, OnDestroy, ViewChild, ElementRef } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
 import { Router, RouterLink } from '@angular/router';
+import { Subscription } from 'rxjs';
 // Components
 import { InputAnimateComponent } from 'src/app/components/buttons/input-animate/input-animate.component';
+import { LoadingComponent } from 'src/app/components/others/loading/loading.component';
+import { ApiService, ApiResponse } from 'src/services/api.service';
+import { CapacitorPreferencesService } from 'src/services/capacitorPreferences.service';
+import { WebSocketService } from 'src/services/webSocket.service';
 
 @Component({
   selector: 'app-chat-view',
   templateUrl: './chat-view.page.html',
   styleUrls: ['./chat-view.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, InputAnimateComponent, RouterLink]
+  imports: [
+    IonContent,
+    IonHeader,
+    IonTitle,
+    IonToolbar,
+    CommonModule,
+    FormsModule,
+    InputAnimateComponent,
+    RouterLink,
+    LoadingComponent
+  ]
 })
-export class ChatViewPage implements OnInit {
-
-  constructor(private router: Router) { }
+export class ChatViewPage implements OnInit, OnDestroy {
+  private subscription: Subscription = new Subscription;
+  url_profile_pic: string = '';
   messages: { text: string, sent: boolean, loading?: boolean }[] = [];
+  isLoading: boolean = false;
   @ViewChild('chatContainer') chatContainer!: ElementRef;
 
-  ngOnInit(): void {
-    setInterval(() => {
-      this.receiveMessage();
-    }, 6000);
+  constructor(
+    private router: Router,
+    private apiService: ApiService,
+    private capacitorPreferencesService: CapacitorPreferencesService,
+    private webSocketService: WebSocketService
+  ) { }
+
+  async ngOnInit() {
+    try {
+      this.isLoading = true;
+
+      const getUrlProfilePicResponse = await this.capacitorPreferencesService.getUserURLProfilePic();
+      this.url_profile_pic = getUrlProfilePicResponse ? getUrlProfilePicResponse : '';
+
+      const token = await this.capacitorPreferencesService.getToken();
+
+      if(token) {
+        const getMessagesResponse = await this.getMessagesByUser(token);
+
+        if (getMessagesResponse.status !== 200) return console.error('Error getting messages');
+
+        for(let message of getMessagesResponse.data){
+          if (message.con_message) {
+            this.messages.push({ text: message.con_message, sent: true });
+            if (message.con_response) this.messages.push({ text: message.con_response, sent: false });
+          }
+        }
+
+        this.webSocketService.connect(token);
+        this.subscription = this.webSocketService.messages$.subscribe((message) => {
+          console.log('Message received:', message);
+          this.messages.push({ text: message, sent: false });
+          this.scrollToBottom();
+        });
+      } else return console.error('No token found');
+    } catch (error) {
+      console.error(error);
+    } finally {
+      this.isLoading = false;
+    }
+  }
+
+  ngOnDestroy(): void {
+    if (this.subscription) this.subscription.unsubscribe();
+    this.webSocketService.closeConnection();
   }
 
   handleSend(message: string): void {
@@ -30,47 +87,35 @@ export class ChatViewPage implements OnInit {
       // Encuentra la posición del mensaje en carga, si existe
       const loadingIndex = this.messages.findIndex(message => message.loading);
 
-      if (loadingIndex !== -1) {
-        // Inserta el mensaje enviado antes del mensaje en carga
-        this.messages.splice(loadingIndex, 0, { text: message, sent: true });
-      } else {
-        // Agrega el mensaje al final si no hay mensaje en carga
-        this.messages.push({ text: message, sent: true });
-      }
+      // Inserta el mensaje enviado antes del mensaje en carga
+      if (loadingIndex !== -1) this.messages.splice(loadingIndex, 0, { text: message, sent: true });
+      // Agrega el mensaje al final si no hay mensaje en carga
+      else this.messages.push({ text: message, sent: true });
+
+      console.log('Message sent:', message);
+
+      this.webSocketService.sendMessage(message);
+
       this.scrollToBottom();
     }
   }
 
-  receiveMessage(): void {
-    // Simula la llegada del mensaje con un indicador de carga
-    this.messages.push({ text: '', sent: false, loading: true });
-    this.scrollToBottom();
-
-    setTimeout(() => {
-      const receivedMessages = [
-        '¿Cómo has estado? ¿De qué te gustaría hablar el día de hoy?',
-        '¡Hola! ¿Qué piensas tú sobre los avances tecnológicos en la actualidad?',
-        '¿Qué opinas sobre la educación en línea?',
-        '¿Cuál es tu opinión sobre la inteligencia artificial?',
-        '¿Qué piensas sobre la automatización de procesos?',
-        '¿Qué opinas sobre la programación?',
-      ];
-      const randomMessage = receivedMessages[Math.floor(Math.random() * receivedMessages.length)];
-      const loadingMessageIndex = this.messages.findIndex(message => message.loading);
-
-      if (loadingMessageIndex !== -1) {
-        this.messages[loadingMessageIndex] = { text: randomMessage, sent: false, loading: false };
-        this.scrollToBottom();
-      }
-    }, 5000);
-  }
-
-  scrollToBottom(): void {
+  private scrollToBottom(): void {
     try {
       this.chatContainer.nativeElement.scrollTop = this.chatContainer.nativeElement.scrollHeight;
     } catch (err) { }
   }
+
   back(){
     this.router.navigate(['/home']);
+  }
+
+  private async getMessagesByUser(token: string) {
+    const response: ApiResponse = await this.apiService.get(
+      '/ia/get-messages-by-user/',
+      [['Authorization', `Bearer ${token}`]]
+    );
+
+    return response;
   }
 }
---
+++ src/services/webSocket.service.ts
@@ -0,0 +1,49 @@
+import { Injectable, OnDestroy } from '@angular/core';
+import { Subject, Observable, map } from 'rxjs';
+import { webSocket, WebSocketSubject } from 'rxjs/webSocket';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class WebSocketService implements OnDestroy {
+  private socket$!: WebSocketSubject<any>;
+  private messagesSubject$ = new Subject<any>();
+  public messages$ = this.messagesSubject$.asObservable();
+
+  private baseSocketUrl = 'wss://wg-api-linux2.onrender.com/ws/chat/?token=';
+
+  constructor() {}
+
+  public connect(token: string): void {
+    if (!token) {
+      console.error('Token is required to establish a WebSocket connection.');
+      return;
+    }
+
+    const socketUrl = `${this.baseSocketUrl}${token}`;
+    this.socket$ = webSocket(socketUrl);
+
+    this.socket$.subscribe({
+      next: (message) => this.messagesSubject$.next(message),
+      error: (err) => console.error('WebSocket Error:', err),
+      complete: () => console.warn('WebSocket connection closed')
+    });
+
+  }
+
+  public sendMessage(message: any): void {
+    if (this.socket$) {
+      this.socket$.next(message);
+    }
+  }
+
+  public closeConnection(): void {
+    if (this.socket$) {
+      this.socket$.complete();
+    }
+  }
+
+  ngOnDestroy() {
+    this.closeConnection();
+  }
+}
