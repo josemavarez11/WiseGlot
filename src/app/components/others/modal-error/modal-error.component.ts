3d7fd80800443c6a8f2744dd03e0cfbd86ba6ded
Misa24jr <misa24jr@gmail.com>
2024-09-02 16:38:24 -0400

    modal de error

--- src/app/components/others/modal-error/modal-error.component.ts
+++ src/app/components/others/modal-error/modal-error.component.ts
@@ -13,18 +13,13 @@ export class ModalErrorComponent implements OnInit {
   @Input() isVisible = false;
   @Input() title = '';
   @Input() option = '';
-  @Input() redirectTo = ''; // Nueva propiedad para la ruta de redirección
 
   constructor(private router: Router) { }
 
-  async ngOnInit() {
-    if (this.isVisible) {
-      setTimeout(() => {
-        this.isVisible = false;
-        if (this.redirectTo) {
-          this.router.navigate([this.redirectTo]);
-        }
-      }, 2000); // 2 segundos
-    }
+  ngOnInit() {
+  }
+
+  closeModal(){
+    this.isVisible = false;
   }
 }
